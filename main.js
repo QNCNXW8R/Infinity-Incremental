c01c95df69b986067d2c1b764360457b2a370bc9
QNCNXW8R <danielrcgray@gmail.com>
2017-09-21 08:34:15 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,59 +1,143 @@
+var pointClickBase = 1;
+var pointClickMult = 1;
 var points = 0;
 var pointMakers = 0;
 var pointMakerMult = 1;
 var dotMakers = 0;
+
 var upgradeList = [
-['upgrade1',true,increasePointMakerMult,2,false],
-['upgrade2',true,increasePointMakerMult,3,false],
+  pointClickBaseUpgrade1 = {
+    name:'Bigger Clicks I',
+    visible:true,
+    cost:10,
+    currency:'points',
+    func:increasePointClickBase,
+    value:1,
+    desc:'Increases point click value by 1.',
+    purchased:false
+  },
+  pointClickBaseUpgrade2 = {
+    name:'Bigger Clicks II',
+    visible:true,
+    cost:10,
+    currency:'points',
+    func:increasePointClickBase,
+    value:1,
+    desc:'Increases point click value by 1.',
+    purchased:false
+  },
+  pointClickBaseUpgrade3 = {
+    name:'Bigger Clicks III',
+    visible:true,
+    cost:10,
+    currency:'points',
+    func:increasePointClickBase,
+    value:1,
+    desc:'Increases point click value by 1.',
+    purchased:false
+  },
+  pointClickBaseUpgrade4 = {
+    name:'Bigger Clicks IV',
+    visible:true,
+    cost:10,
+    currency:'points',
+    func:increasePointClickBase,
+    value:1,
+    desc:'Increases point click value by 1.',
+    purchased:false
+  },
+  pointMakerUpgrade1 = {
+    name:'Pointy Point Makers',
+    visible:true,
+    cost:10,
+    currency:'points',
+    func:increasePointMakerMult,
+    value:2,
+    desc:'Doubles the production of Point Makers.',
+    purchased:false
+  },
+  pointMakerUpgrade2 = {
+    name:'Very Pointy Point Makers',
+    visible:true,
+    cost:20,
+    currency:'points',
+    func:increasePointMakerMult,
+    value:3,
+    desc:'Triples the production of Point Makers.',
+    purchased:false
+  }
 ];
 
+function pointClick(){
+  points = points + (pointClickBase * pointClickMult);
+  document.getElementById('pointAmount').innerHTML = points;
+};
 
-function pointClick(number){
-    points = points + number;
-    document.getElementById('pointAmount').innerHTML = points;
+function pointAdd(number){
+  points = points + number;
+  document.getElementById('pointAmount').innerHTML = points;
 };
 
 function buyPointMaker(){
-    var pointMakerCost = Math.floor(10 * Math.pow(1.1, pointMakers));
-    if(points >= pointMakerCost){
-        pointMakers = pointMakers + 1;
-        points = points - pointMakerCost
-        document.getElementById('pointMakerAmount').innerHTML = pointMakers;
-        document.getElementById('pointAmount').innerHTML = points;
-    };
-    var nextPointMakerCost = Math.floor(10 * Math.pow(1.1, pointMakers));
-    document.getElementById('pointMakerCost').innerHTML = nextPointMakerCost;
+  var pointMakerCost = Math.floor(10 * Math.pow(1.1, pointMakers));
+  if (points >= pointMakerCost){
+    pointMakers = pointMakers + 1;
+    points = points - pointMakerCost
+    document.getElementById('pointMakerAmount').innerHTML = pointMakers;
+    document.getElementById('pointAmount').innerHTML = points;
+  };
+  var nextPointMakerCost = Math.floor(10 * Math.pow(1.1, pointMakers));
+  document.getElementById('pointMakerCost').innerHTML = nextPointMakerCost;
 };
 
 function buyDotMaker(){
-    var dotMakerCost = Math.floor(100 * Math.pow(1.1, dotMakers));
-    if(points >= dotMakerCost){
-        dotMakers = dotMakers + 1;
-        points = points - dotMakerCost
-        document.getElementById('dotMakerAmount').innerHTML = dotMakers;
-        document.getElementById('pointAmount').innerHTML = points;
-    };
-    var nextDotMakerCost = Math.floor(100 * Math.pow(1.1, dotMakers));
-    document.getElementById('dotMakerCost').innerHTML = nextDotMakerCost;
+  var dotMakerCost = Math.floor(100 * Math.pow(1.1, dotMakers));
+  if (points >= dotMakerCost){
+  dotMakers = dotMakers + 1;
+  points = points - dotMakerCost;
+  document.getElementById('dotMakerAmount').innerHTML = dotMakers;
+  document.getElementById('pointAmount').innerHTML = points;
+  };
+  var nextDotMakerCost = Math.floor(100 * Math.pow(1.1, dotMakers));
+  document.getElementById('dotMakerCost').innerHTML = nextDotMakerCost;
+};
+
+function increasePointClickBase(number){
+  pointClickBase = pointClickBase + number
+};
+
+function increasePointClickMult(number){
+  pointClickMult = pointClickMult * number
 };
 
 function increasePointMakerMult(number){
-    pointMakerMult = pointMakerMult * number
+  pointMakerMult = pointMakerMult * number;
 };
 
 function updateUpgradeBox(){
-    var content = ''
-    for (i = 0; i < upgradeList.length; i++){
-        if(upgradeList[i][1]){
-            content = content + upgradeList[i][0] + '<button type="button"; onClick="upgradeList[' + i + '][4]=true">Buy</button><br />'
-        };
+  var content = '';
+  for (i in upgradeList){
+    if (upgradeList[i].visible == true && upgradeList[i].purchased == false) {
+      u = upgradeList[i]
+      content = content + u.name + ' <button type="button"; onClick="purchaseUpgrade(upgradeList[';
+      content = content + i + '])">Buy (' + u.cost + ' ' + u.currency + ')</button> ' + u.desc + '<br />';
     };
-    console.log(content)
-    document.getElementById('upgradeBox').innerHTML = content;
+  };
+  document.getElementById('upgradeBox').innerHTML = content;
+};
 
+function purchaseUpgrade(upgrade){
+  if (upgrade.cost <= window[upgrade.currency]){
+    window[upgrade.currency] -= upgrade.cost;
+    upgrade.func(upgrade.value);
+    upgrade.purchased = true;
+    updateUpgradeBox();
+    pointAdd(0);
+  };
 };
 
 window.setInterval(function(){
-    pointClick(pointMakers);
-    pointClick(dotMakers * 10)
+  updateUpgradeBox();
+  pointAdd(pointMakers * pointMakerMult);
+  pointAdd(dotMakers * 10);
 }, 1000);
